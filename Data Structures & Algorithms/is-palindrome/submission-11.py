1d13f69655d0167cceb1e7341cee9d8388b191dd
Brandon Hernandez-Cano <brandon.z.cano@gmail.com>
2026-09-09 13:13:24 -0700

    Add: is-palindrome - submission-11

---
+++ Data Structures & Algorithms/is-palindrome/submission-11.py
@@ -0,0 +1,23 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+
+        leftPointer = 0
+        rightPointer = len(s) - 1
+
+        while leftPointer < rightPointer :
+            
+            while not s[leftPointer].isalnum() and leftPointer < rightPointer:
+                leftPointer+=1
+            
+            while not s[rightPointer].isalnum() and rightPointer > leftPointer:
+                rightPointer-=1
+            
+
+            if s[leftPointer].lower() != s[rightPointer].lower():
+                return False
+            
+            leftPointer+=1
+            rightPointer-=1
+        
+        return True
+            
